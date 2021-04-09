9b5913ecc011f0c6e86c9d70aac2b0d59558e2dc
Anurag Kulkarni <kanurag0801@gmail.com>
2021-04-09 23:46:43 +0530

    Comments Added

--- tokens/loginToken.js
+++ tokens/loginToken.js
@@ -1,8 +1,12 @@
+//Importing JWT module
 const jwt = require('jsonwebtoken');
 
+//Creating a function to verify the token
 function verifyToken (req,res,next){
     
     const token = req.query.token;
+    
+    //If token is not present then access will be denied
     if(!token) return res.status(400).json("Access Denied");
 
     try {
